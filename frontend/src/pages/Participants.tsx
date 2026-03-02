cd3c5f7e29d1f9eb25c0d56d725bfd2fdeb6e211
Vanshu Galhotra <vanshugalhotra3332@gmail.com>
2026-03-02 15:36:46 +0530

    chors

--- frontend/src/pages/Participants.tsx
+++ frontend/src/pages/Participants.tsx
@@ -63,7 +63,7 @@ export default function Participants() {
 
       const response = await participantService.getAll({
         search: search,
-        take: 50,
+        take: 150,
         includeRelations: true,
         filters: JSON.stringify(activeFilters),
       });
